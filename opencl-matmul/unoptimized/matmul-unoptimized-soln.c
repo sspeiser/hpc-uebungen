5365b7316723fb60d8a699ed5bcea767bab27ab1
speiser <sebastian.speiser@hft-stuttgart.de>
2022-12-12 11:03:28 +0100

    Win requires platforms openCL

--- opencl-matmul/unoptimized/matmul-unoptimized-soln.c
+++ opencl-matmul/unoptimized/matmul-unoptimized-soln.c
@@ -3,8 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#include <unistd.h>
-#include <sys/time.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #ifdef __APPLE__
@@ -15,26 +13,27 @@
 
 #define N 1024
 #define MATRIX_SIZE (N * N)
-#define MATRIX_MEM  (N * N * sizeof(float))
+#define MATRIX_MEM (N * N * sizeof(float))
 
 #define CL_PROGRAM_FILE "matmul-unoptimized-soln.cl"
 #define KERNEL_NAME "matmul_unoptimized"
 //#define KERNEL_NAME "matmul_localvar"
 
+
 #define MAX_PLATFORMS 10
 #define MAX_DEVICES 10
 #define MAX_NAME_LENGTH 128
 
-struct benchmark_result {
+struct benchmark_result
+{
     double transfer_time;
     double calc_time;
     int errors;
 };
 
-
-int benchmark(cl_device_id device, char *program_text, char *kernel_name, struct benchmark_result *result) 
+int benchmark(cl_device_id device, char *program_text, char *kernel_name, struct benchmark_result *result)
 {
-    // Create a compute context 
+    // Create a compute context
     cl_context context;
     context = clCreateContext(0, 1, &device, NULL, NULL, NULL);
     if (!context)
@@ -51,9 +50,9 @@ int benchmark(cl_device_id device, char *program_text, char *kernel_name, struct
         return -1;
     }
 
-    int err;                
-    cl_program program = clCreateProgramWithSource(context, 1, (const char **) &program_text, NULL, &err);
-    if(err < 0)
+    int err;
+    cl_program program = clCreateProgramWithSource(context, 1, (const char **)&program_text, NULL, &err);
+    if (err < 0)
     {
         fprintf(stderr, "Failed to create program.\n");
         return -1;
@@ -70,7 +69,6 @@ int benchmark(cl_device_id device, char *program_text, char *kernel_name, struct
         fprintf(stderr, "%s\n", buffer);
         return -1;
     }
-    
 
     // Create Kernels
     cl_kernel kernel = clCreateKernel(program, kernel_name, &err);
@@ -84,7 +82,7 @@ int benchmark(cl_device_id device, char *program_text, char *kernel_name, struct
     float *h_a = malloc(MATRIX_MEM);
     float *h_b = malloc(MATRIX_MEM);
     float *h_c = malloc(MATRIX_MEM);
-    for(size_t i=0; i<MATRIX_SIZE;i++) 
+    for (size_t i = 0; i < MATRIX_SIZE; i++)
     {
         h_a[i] = 2.0;
         h_b[i] = 4.0;
@@ -95,7 +93,7 @@ int benchmark(cl_device_id device, char *program_text, char *kernel_name, struct
     cl_mem d_b = clCreateBuffer(context, CL_MEM_READ_ONLY, MATRIX_MEM, NULL, NULL);
     cl_mem d_c = clCreateBuffer(context, CL_MEM_READ_WRITE, MATRIX_MEM, NULL, NULL);
 
-    if(!d_a | !d_b | !d_c)
+    if (!d_a | !d_b | !d_c)
     {
         fprintf(stderr, "Failed to allocate memory on device\n");
         return -1;
@@ -106,7 +104,7 @@ int benchmark(cl_device_id device, char *program_text, char *kernel_name, struct
     cl_event prof_event;
     cl_ulong start_time, end_time;
     size_t return_bytes;
-   
+
     err = clEnqueueWriteBuffer(commands, d_a, CL_TRUE, 0, MATRIX_MEM, h_a, 0, NULL, &prof_event);
     if (err != CL_SUCCESS)
     {
@@ -115,11 +113,11 @@ int benchmark(cl_device_id device, char *program_text, char *kernel_name, struct
     }
     // Wait for transfer to finish
     clFinish(commands);
-    
+
     clGetEventProfilingInfo(prof_event, CL_PROFILING_COMMAND_START, sizeof(cl_ulong), &start_time, &return_bytes);
     clGetEventProfilingInfo(prof_event, CL_PROFILING_COMMAND_END, sizeof(cl_ulong), &end_time, &return_bytes);
-    transfer_sec += (double) (end_time - start_time) / 1.0e9;
-    
+    transfer_sec += (double)(end_time - start_time) / 1.0e9;
+
     err = clEnqueueWriteBuffer(commands, d_b, CL_TRUE, 0, MATRIX_MEM, h_b, 0, NULL, &prof_event);
     if (err != CL_SUCCESS)
     {
@@ -128,15 +126,14 @@ int benchmark(cl_device_id device, char *program_text, char *kernel_name, struct
     }
     // Wait for transfer to finish
     clFinish(commands);
-    
+
     clGetEventProfilingInfo(prof_event, CL_PROFILING_COMMAND_START, sizeof(cl_ulong), &start_time, &return_bytes);
     clGetEventProfilingInfo(prof_event, CL_PROFILING_COMMAND_END, sizeof(cl_ulong), &end_time, &return_bytes);
-    transfer_sec += (double) (end_time - start_time) / 1.0e9;
-    
-    
+    transfer_sec += (double)(end_time - start_time) / 1.0e9;
+
     // Set the arguments to our compute kernel
     int n = N;
-    err  = clSetKernelArg(kernel, 0, sizeof(int), &n);
+    err = clSetKernelArg(kernel, 0, sizeof(int), &n);
     err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &d_a);
     err |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &d_b);
     err |= clSetKernelArg(kernel, 3, sizeof(cl_mem), &d_c);
@@ -157,11 +154,11 @@ int benchmark(cl_device_id device, char *program_text, char *kernel_name, struct
 
     // Wait for the commands to get serviced before reading back results
     clFinish(commands);
-    
+
     clGetEventProfilingInfo(prof_event, CL_PROFILING_COMMAND_START, sizeof(cl_ulong), &start_time, &return_bytes);
     clGetEventProfilingInfo(prof_event, CL_PROFILING_COMMAND_END, sizeof(cl_ulong), &end_time, &return_bytes);
-    double calc_time = (double) (end_time - start_time) / 1.0e9;
-      
+    double calc_time = (double)(end_time - start_time) / 1.0e9;
+
     // Copy Result Data Back
     err = clEnqueueReadBuffer(commands, d_c, CL_TRUE, 0, MATRIX_MEM, h_c, 0, NULL, &prof_event);
     if (err != CL_SUCCESS)
@@ -171,15 +168,17 @@ int benchmark(cl_device_id device, char *program_text, char *kernel_name, struct
     }
     // Wait for transfer to finish
     clFinish(commands);
-    
+
     clGetEventProfilingInfo(prof_event, CL_PROFILING_COMMAND_START, sizeof(cl_ulong), &start_time, &return_bytes);
     clGetEventProfilingInfo(prof_event, CL_PROFILING_COMMAND_END, sizeof(cl_ulong), &end_time, &return_bytes);
-    transfer_sec += (double) (end_time - start_time) / 1.0e9;
+    transfer_sec += (double)(end_time - start_time) / 1.0e9;
 
     // Check Results
     int errors = 0;
-    for(int i=0; i<MATRIX_SIZE;i++) {
-        if(fabs(h_c[i] - N * 2.0 * 4.0) > 0.0001) {
+    for (int i = 0; i < MATRIX_SIZE; i++)
+    {
+        if (fabs(h_c[i] - N * 2.0 * 4.0) > 0.0001)
+        {
             errors += 1;
         }
     }
@@ -201,61 +200,96 @@ int benchmark(cl_device_id device, char *program_text, char *kernel_name, struct
     return 0;
 }
 
-
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    // Get all devices
-    cl_device_id *devices;
-    cl_uint n_devices;
-    clGetDeviceIDs(NULL, CL_DEVICE_TYPE_ALL, MAX_DEVICES, NULL, &n_devices);
-    devices = (cl_device_id *) malloc(n_devices * sizeof(cl_device_id));
-    clGetDeviceIDs(NULL, CL_DEVICE_TYPE_ALL, n_devices, devices, NULL);
-    if(n_devices == 0) {
-        fprintf(stderr, "No devices found. Exiting.\n");
+    int err;
+    // Get all platforms
+    cl_platform_id *platforms;
+    cl_uint n_platforms;
+
+    err = clGetPlatformIDs(16, NULL, &n_platforms);
+    if (err != CL_SUCCESS)
+    {
+        fprintf(stderr, "Error getting number of platforms\n");
         return -1;
     }
-
-    char name[MAX_NAME_LENGTH];
-    printf("Devices:\n");
-    for(int i=0;i<n_devices;i++) {
-        clGetDeviceInfo(devices[i], CL_DEVICE_NAME, sizeof(name), name, NULL);
-        printf("[%d]: %s\n", i, name);
-    }
-
-    // Load program from file
-    FILE *program_file = fopen(CL_PROGRAM_FILE, "r");
-    if(program_file == NULL) {
-        fprintf(stderr, "Failed to open OpenCL program file\n");
+    if (n_platforms == 0)
+    {
+        fprintf(stderr, "No platforms found\n");
         return -1;
     }
-    fseek(program_file, 0, SEEK_END);
-    size_t program_size = ftell(program_file);
-    rewind(program_file);
-    char *program_text = (char *) malloc((program_size + 1) * sizeof(char));
-    program_text[program_size] = '\0';
-    fread(program_text, sizeof(char), program_size, program_file);
-    fclose(program_file);
-    
-
-    printf("Device                                        | GFLOP/s w/o transfer | GFLOP/s w/ transfer |      Errors\n");
-    printf("--------------------------------------------------------------------------------------------------------\n");
-    for(int i=0; i<n_devices;i++) {
-        clGetDeviceInfo(devices[i], CL_DEVICE_NAME, sizeof(name), name, NULL);
-        printf("[%d]: %40s | ", i, name);
-
-        struct benchmark_result result;
-
-        benchmark(devices[i], program_text, KERNEL_NAME, &result);
-
-        double gflops_calc = 2.0 * N * N * N / 1e9 / result.calc_time;
-        double gflops_transfer = 2.0 * N * N * N / 1e9 / (result.transfer_time + result.calc_time);
-        
-        printf("          %10.2f |          %10.2f |   %9d\n", gflops_calc, gflops_transfer, result.errors);
+    platforms = (cl_platform_id *)malloc(n_platforms * sizeof(cl_platform_id));
+    err = clGetPlatformIDs(n_platforms, platforms, NULL);
+    if (err != CL_SUCCESS)
+    {
+        fprintf(stderr, "Error getting platforms\n");
+        return -1;
     }
-      
-    
-    free(program_text);
-    free(devices);
+    char platform_name[MAX_NAME_LENGTH];
+    printf("Platforms:\n");
+    for (int i = 0; i < n_platforms; i++)
+    {
+        clGetPlatformInfo(platforms[i], CL_PLATFORM_NAME, sizeof(platform_name), platform_name, NULL);
+        printf("[%d]: %s\n", i, platform_name);
+    }
+
+    for (int j = 0; j < n_platforms; j++)
+    {
+
+        // Get all devices
+        cl_device_id *devices;
+        cl_uint n_devices;
+        clGetDeviceIDs(platforms[j], CL_DEVICE_TYPE_ALL, MAX_DEVICES, NULL, &n_devices);
+        devices = (cl_device_id *)malloc(n_devices * sizeof(cl_device_id));
+        clGetDeviceIDs(platforms[j], CL_DEVICE_TYPE_ALL, n_devices, devices, NULL);
+        if (n_devices == 0)
+        {
+            fprintf(stderr, "No devices found. Exiting.\n");
+            return -1;
+        }
+
+        char name[MAX_NAME_LENGTH];
+        printf("Devices:\n");
+        for (int i = 0; i < n_devices; i++)
+        {
+            clGetDeviceInfo(devices[i], CL_DEVICE_NAME, sizeof(name), name, NULL);
+            printf("[%d]: %s\n", i, name);
+        }
+
+        // Load program from file
+        FILE *program_file = fopen(CL_PROGRAM_FILE, "r");
+        if (program_file == NULL)
+        {
+            fprintf(stderr, "Failed to open OpenCL program file\n");
+            return -1;
+        }
+        fseek(program_file, 0, SEEK_END);
+        size_t program_size = ftell(program_file);
+        rewind(program_file);
+        char *program_text = (char *)malloc((program_size + 1) * sizeof(char));
+        program_text[program_size] = '\0';
+        fread(program_text, sizeof(char), program_size, program_file);
+        fclose(program_file);
+
+        printf("Device                                        | GFLOP/s w/o transfer | GFLOP/s w/ transfer |      Errors\n");
+        printf("--------------------------------------------------------------------------------------------------------\n");
+        for (int i = 0; i < n_devices; i++)
+        {
+            clGetDeviceInfo(devices[i], CL_DEVICE_NAME, sizeof(name), name, NULL);
+            printf("[%d]: %40s | ", i, name);
+
+            struct benchmark_result result;
+
+            benchmark(devices[i], program_text, KERNEL_NAME, &result);
+
+            double gflops_calc = 2.0 * N * N * N / 1e9 / result.calc_time;
+            double gflops_transfer = 2.0 * N * N * N / 1e9 / (result.transfer_time + result.calc_time);
+
+            printf("          %10.2f |          %10.2f |   %9d\n", gflops_calc, gflops_transfer, result.errors);
+        }
+
+        free(program_text);
+        free(devices);
+    }
     return 0;
 }
-
