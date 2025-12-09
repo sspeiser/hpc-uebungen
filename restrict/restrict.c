022621c73e85705af1b81150c3506e5962af09d8
speiser <sebastian.speiser@hft-stuttgart.de>
2025-12-09 09:17:32 +0100

    restrict example

---
+++ restrict/restrict.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int shift(int* restrict a, int* restrict b , int n) {
+    for(int i=1; i<n; i++) {
+        a[i] = b[i-1];
+    }
+    return a[n-1];
+}
+
+int main() {
+    int* a = malloc(sizeof(int) * 1024);
+    // int* b = malloc(sizeof(int) * 1024);
+    int* b = a;
+    int n = 1024;
+    for(int i = 0; i < n; i ++) {
+        a[i] = 0;
+        b[i] = 0;
+    }
+    b[0] = 1000;
+    b[n-2] = 2000;
+
+    int result = shift(a, b, n);
+    printf("%d\n", result);
+}
